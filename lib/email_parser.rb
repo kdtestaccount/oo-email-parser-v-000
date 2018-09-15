c7610952cf41b8055bfe5e3dcb2e709e5a1107a0
kvdoherty <kvdoherty@gmail.com>
2018-09-15 18:33:07 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -18,8 +18,8 @@ class EmailParser
   
   def parse 
     if @emails.include?(',')
-      email_address  = @emails.split( ' ' ).join(" , ")
-      @emails_array << email_address
+      @emails =  @emails.split( ' ' ).join(" , ")
+      # @emails_array << email_address
     else
       email_address  = @emails.split( ' ' ).join(" , ")
 
