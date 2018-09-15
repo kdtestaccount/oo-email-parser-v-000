1c24212398c43414ed96d3b610b0d706e30a5e16
kvdoherty <kvdoherty@gmail.com>
2018-09-15 19:37:02 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,31 +15,17 @@ class EmailParser
     @emails_array = []
   end
   
-      it "parses CSV emails" do
-      expect(EmailParser.new("avi@test.com, arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
-    end
-
-    it "parses space delimited emails" do
-      expect(EmailParser.new("avi@test.com arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
-    end
-
-    it "parses both CSV and space delimited emails" do
+      expect(EmailParser.new("avi@test.com, arel@test.com").parse).to eq(["avi@test.com", 
+      expect(EmailParser.new("avi@test.com arel@test.com").parse).to eq(["avi@test.com", 
       emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
-      expect(EmailParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"]) 
-    end
 
-    it 'parses and removes duplicate emails' do
       expect(EmailParser.new("avi@test.com, avi@test.com").parse).to eq(["avi@test.com"])
-    end
-  end
-end
 
-  
+
   
   def parse 
     
-    if @emails.include?(', ')
-      @emails_array =  @emails.split( ', ' )
+      @emails_array =  @emails.split( ' ' )
     elsif @emails.include?('","')
       @emails_array =  @emails.split( '","' )
     else
